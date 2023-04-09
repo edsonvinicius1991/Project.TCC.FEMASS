ec5b54bb2ab8e11cdf95d9dd008dd12456b5bdfe
Edson Vinicius Oliveira <edson.vinicius@hotmail.com>
2023-04-09 00:54:07 -0300

    Debug erro fixed;

--- project-tcc/src/main/java/com/project/tcc/projecttcc/ProjectTccApplication.java
+++ project-tcc/src/main/java/com/project/tcc/projecttcc/ProjectTccApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProjectTccApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectTccApplication.class, args);
 	}
