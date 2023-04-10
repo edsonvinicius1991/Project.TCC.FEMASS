d05daa892a304ecbc52e525043898a3e96909c24
Edson Vinicius Oliveira <edson.vinicius@hotmail.com>
2023-04-10 01:09:09 -0300

    First endpoint

--- project-tcc/src/main/java/com/project/tcc/projecttcc/controller/EquipmentController.java
+++ project-tcc/src/main/java/com/project/tcc/projecttcc/controller/EquipmentController.java
@@ -1,14 +1,23 @@
 package com.project.tcc.projecttcc.controller;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.project.tcc.projecttcc.dtos.EquipmentDto;
+import com.project.tcc.projecttcc.model.EquipmentModel;
 import com.project.tcc.projecttcc.services.EquipmentService;
 
 @RestController
-@CrossOrigin(origins = "*", maxAge = 3600)
-@RequestMapping("/equipment")
+@CrossOrigin(origins = "*", maxAge = 3600) //CORS is a mechanism that allows a server to specify who can access its resources. The snippet you provided sets the origin to allow any domain (the "*" wildcard)
+@RequestMapping ("/equipment")
 
 public class EquipmentController {
     
@@ -17,6 +26,15 @@ public class EquipmentController {
     public EquipmentController(EquipmentService equipmentService) {
         this.equipmentService = equipmentService;
     }
-
     
+    
+    @PostMapping ("/add")
+    public ResponseEntity<Object> saveEquipment(@RequestBody @Valid EquipmentDto equipmentDto){
+
+        var equipmentModel = new EquipmentModel();
+        BeanUtils.copyProperties(equipmentDto, equipmentModel);
+        
+        return ResponseEntity.status(HttpStatus.CREATED).body(((Object) equipmentService.save(equipmentModel)));
+
+    }    
 }
--- project-tcc/src/main/java/com/project/tcc/projecttcc/services/EquipmentService.java
+++ project-tcc/src/main/java/com/project/tcc/projecttcc/services/EquipmentService.java
@@ -1,6 +1,10 @@
 package com.project.tcc.projecttcc.services;
 
+import javax.transaction.Transactional;
+
 import org.springframework.stereotype.Service;
+
+import com.project.tcc.projecttcc.model.EquipmentModel;
 import com.project.tcc.projecttcc.repositories.EquipmentRepository;
 
 @Service
@@ -12,4 +16,8 @@ public class EquipmentService {
         this.equipmentRepository = equipmentRepository;
     }
     
+    @Transactional //"@Transactional" is a Java annotation that is used to manage transactions in a Spring-based application. The annotation marks a method, or a class, as transactional, which means that if an error occurs during the execution of any database-related operations within the method, the whole transaction will be rolled back to its previous state. This ensures database consistency and helps to prevent data corruption.
+    public EquipmentModel save(EquipmentModel equipmentModel){
+        return equipmentRepository.save(equipmentModel);
+    }
 }
