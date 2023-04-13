da53496fd5e4b7dbc9ec3d9bcfd59fd454102ee4
Edson Vinicius Oliveira <edson.vinicius@hotmail.com>
2023-04-12 22:30:33 -0300

    Container Endpoints

--- project-tcc/src/main/java/com/project/tcc/projecttcc/controller/ContainerController.java
+++ project-tcc/src/main/java/com/project/tcc/projecttcc/controller/ContainerController.java
@@ -1,9 +1,25 @@
 package com.project.tcc.projecttcc.controller;
 
+import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.project.tcc.projecttcc.dtos.ContainerDto;
+import com.project.tcc.projecttcc.model.ContainerModel;
 import com.project.tcc.projecttcc.services.ContainerService;
 
 @RestController
@@ -18,5 +34,54 @@ public class ContainerController {
         this.containerService = containerService;
     }
 
+    @PostMapping ("/add")
+    public ResponseEntity<Object> saveContainer(@RequestBody @Valid ContainerDto containerDto){
+
+        var containerModel = new ContainerModel();
+        BeanUtils.copyProperties(containerDto, containerModel);
+        
+        return ResponseEntity.status(HttpStatus.CREATED).body(((Object) containerService.save(containerModel)));
+
+    }
     
+    @GetMapping
+    public ResponseEntity<List<ContainerModel>> getAllContainer(){
+        return ResponseEntity.status(HttpStatus.OK).body(containerService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> getIdContainer(@PathVariable(value = "id")String idContainer){
+        Optional<ContainerModel> containerModelOptional = containerService.findById(idContainer);
+        if(!containerModelOptional.isPresent()){ //Case: If id not found!
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Container not found.");
+        }
+        
+        return ResponseEntity.status(HttpStatus.OK).body(containerModelOptional.get());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deleteContainers(@PathVariable(value = "id") String idContainer){
+        Optional<ContainerModel> containerModelOptional = containerService.findById(idContainer);
+        if(!containerModelOptional.isPresent()){ //Case: If id not found!
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Container not found.");
+        }
+        
+        containerService.delete(containerModelOptional.get());
+        return ResponseEntity.status(HttpStatus.OK).body("Container deleted sucessfully.");
+
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Object> updateContainers(   @PathVariable(value = "id") String idContainer,
+                                                    @RequestBody @Valid ContainerDto containerDto){
+        Optional<ContainerModel> containerModelOptional = containerService.findById(idContainer);
+        if(!containerModelOptional.isPresent()){ //Se não estiver presente
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Container not found.");
+        }
+        var containerModel = new ContainerModel();
+        BeanUtils.copyProperties(containerDto, containerModel);
+        containerModel.setIdContainer(containerModelOptional.get().getIdContainer());
+        
+        return ResponseEntity.status(HttpStatus.OK).body(containerService.save(containerModel));    
+    }
 }
--- project-tcc/src/main/java/com/project/tcc/projecttcc/services/ContainerService.java
+++ project-tcc/src/main/java/com/project/tcc/projecttcc/services/ContainerService.java
@@ -1,7 +1,15 @@
 package com.project.tcc.projecttcc.services;
 
+import java.util.List;
+import java.util.Optional;
+
+import javax.transaction.Transactional;
+
 import org.springframework.stereotype.Service;
+
+import com.project.tcc.projecttcc.model.ContainerModel;
 import com.project.tcc.projecttcc.repositories.ContainerRepository;
+
 
 @Service
 public class ContainerService {
@@ -12,4 +20,20 @@ public class ContainerService {
         this.containerRepository = containerRepository;
     }
     
+    @Transactional //"@Transactional" is a Java annotation that is used to manage transactions in a Spring-based application. The annotation marks a method, or a class, as transactional, which means that if an error occurs during the execution of any database-related operations within the method, the whole transaction will be rolled back to its previous state. This ensures database consistency and helps to prevent data corruption.
+    public ContainerModel save(ContainerModel containerModel){
+        return containerRepository.save(containerModel);
+    }
+
+    public List<ContainerModel> findAll() {
+        return containerRepository.findAll();
+    }
+
+    public Optional<ContainerModel> findById(String assetId) {
+        return containerRepository.findById(assetId);
+    }
+    @Transactional
+    public void delete(ContainerModel containerModel) {
+        containerRepository.delete(containerModel);
+    }
 }
