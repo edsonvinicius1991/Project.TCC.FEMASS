f0a4ff7be7792c852230b186e1f092c057cf4560
Edson Vinicius Oliveira <edson.vinicius@hotmail.com>
2023-07-27 20:16:56 -0300

    Search by asset added

--- project-tcc-frontend/src/components/equipments/Tabela.js
+++ project-tcc-frontend/src/components/equipments/Tabela.js
@@ -1,20 +1,33 @@
 import "./Equipments.css";
 
-function Tabela({ vetor, handleRemove, handleEdit, handleShow }) {
+import { useState } from 'react';
 
-    const edit = (obj) =>{
+function Tabela({ vetorEquipments, handleRemove, handleEdit, handleShow }) {
+
+    //const equipments = vetorEquipments.map('')
+
+    const edit = (obj) => {
         console.log(obj)
         handleEdit(obj)
     }
 
-    const remove = (id) =>{
+    const remove = (id) => {
         handleRemove(id)
     }
 
+    const [busca, setBusca] = useState('');
 
     return (
 
         <div className="row">
+            <form className="form-inline">
+                <input
+                    className="form-control mr-sm-2"
+                    type="search"
+                    placeholder="Search Asset"
+                    onChange={(e) => setBusca(e.target.value)} />
+                
+            </form>
             <div className="table-responsive ">
                 <table className="table table-striped table-hover table-bordered">
                     <thead>
@@ -30,22 +43,27 @@ function Tabela({ vetor, handleRemove, handleEdit, handleShow }) {
                     </thead>
                     <tbody>
                         {
-                            vetor.map((obj, index)=>(
-                                <tr key={index}>
-                                    <td>{index+1}</td>
-                                    <td>{obj.assetId}</td>
-                                    <td>{obj.description}</td>
-                                    <td>{obj.dueDate}</td>
-                                    <td>{obj.container.idContainer}</td>
-                                    <td>{obj.location.rig}</td>
-                                    <td >
-                                        {/*<a href="#" className="view" title="View" data-toggle="tooltip" style={{ color: "#10ab80" }}><i className="material-icons" >&#xE417;</i></a>*/}
-                                        <button href="#" className="btn btn-primary" id={obj.assetId} onClick={(e) =>{edit(obj, e)}} title="Edit" data-toggle="tooltip" style={{ marginRight: "0.5rem", padding: "0.2rem 0.7rem" }}><i className="material-icons">&#xE254;</i></button>
-                                        <button href="#" className="btn btn-danger" id={obj.assetId} onClick={() =>  {if(window.confirm(`Are you sure to delete the equipment [Asset: ${obj.assetId}] ?`)){remove(obj.assetId)}}} title="Delete" data-toggle="tooltip" style={{ color: "white", marginRight: "0.5rem", padding: "0.2rem 0.7rem" }}><i className="material-icons" >&#xE872;</i></button>
-                                    </td>
-                                </tr>
-                            ))
-                        }                        
+                            vetorEquipments
+                                .filter((obj) => {
+                                    return busca.toLowerCase() === ''
+                                        ? obj
+                                        : obj.assetId.toLowerCase().includes(busca);
+                                }).map((obj, index) => (
+                                    <tr key={index}>
+                                        <td>{index + 1}</td>
+                                        <td>{obj.assetId}</td>
+                                        <td>{obj.description}</td>
+                                        <td>{obj.dueDate}</td>
+                                        <td>{obj.container.idContainer}</td>
+                                        <td>{obj.location.rig}</td>
+                                        <td >
+                                            {/*<a href="#" className="view" title="View" data-toggle="tooltip" style={{ color: "#10ab80" }}><i className="material-icons" >&#xE417;</i></a>*/}
+                                            <button href="#" className="btn btn-primary" id={obj.assetId} onClick={(e) => { edit(obj, e) }} title="Edit" data-toggle="tooltip" style={{ marginRight: "0.5rem", padding: "0.2rem 0.7rem" }}><i className="material-icons">&#xE254;</i></button>
+                                            <button href="#" className="btn btn-danger" id={obj.assetId} onClick={() => { if (window.confirm(`Are you sure to delete the equipment [Asset: ${obj.assetId}] ?`)) { remove(obj.assetId) } }} title="Delete" data-toggle="tooltip" style={{ color: "white", marginRight: "0.5rem", padding: "0.2rem 0.7rem" }}><i className="material-icons" >&#xE872;</i></button>
+                                        </td>
+                                    </tr>
+                                ))
+                        }
                     </tbody>
                 </table>
             </div>
