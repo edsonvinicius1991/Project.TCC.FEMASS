864d4b79d8039a59041b13e751484a7d09990509
Edson Vinicius Oliveira <edson.vinicius@hotmail.com>
2023-10-24 00:35:25 -0300

    Correção de formatação

--- project-tcc-frontend/src/pages/equipments/Equipments.js
+++ project-tcc-frontend/src/pages/equipments/Equipments.js
@@ -1,17 +1,14 @@
 import "./Equipments.css"
 
-import { useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 import { Button } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom'
-
 
 import Tabela from "./Tabela"
 import ModalAdd from "./ModalAdd"
 import ModalEdit from "./ModalEdit";
 
-
-
-function Equipments({filter}) {
+function Equipments({ filter }) {
 
     const navigate = useNavigate()
 
@@ -21,7 +18,7 @@ function Equipments({filter}) {
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
 
-     {/* <!--- ModalEdit---> */ }
+    {/* <!--- ModalEdit---> */ }
     const [showModalEdit, setShowModalEdit] = useState(false);
     const [objSelecionado, setObjSelecionado] = useState(null)
 
@@ -30,9 +27,8 @@ function Equipments({filter}) {
         setObjSelecionado(obj);
         setShowModalEdit(true);
     }
-        
 
-    {/* <!--- Table Equipments---> */ }
+    {/* <!--- Fill table Equipments---> */ }
     const [equipments, setEquipments] = useState([]);
 
     useEffect(() => {
@@ -46,67 +42,56 @@ function Equipments({filter}) {
         fetch(`http://localhost:8080/equipment/${id}`, {
             method: 'DELETE',
             headers: {
-            'Content-Type': 'application/json',
-        },
+                'Content-Type': 'application/json',
+            },
         })
-        .then((resp) => resp.json())
-        .then(
-            navigate(0)
-                        
-        )
-        
-        .catch(err => console.log(err)) 
-
+            .then((resp) => resp.json())
+            .then(
+                navigate(0)
+            )
+            .catch(err => console.log(err))
     }
 
-
-    
-    
-    
     return (
         <div className="container ">
-            
             <h1 style={{ margin: "50px 25px" }}>EQUIPMENTS</h1>
             <div className="crud shadow-lg p-3 mb-5 mt-5 bg-body rounded"  >
                 <div className="row ">
-
                     <div className="col-sm-3 offset-sm-0  mt-5 mb-4 text-gred" >
-                        <Button variant="secondary" onClick={handleShow} style={{ fontSize:"20px"}}>
-                            <i className="bi bi-plus" style={{ fontSize:"30px" , marginRight: "1rem"}}></i>
+                        <Button variant="secondary" onClick={handleShow} style={{ fontSize: "20px" }}>
+                            <i className="bi bi-plus" style={{ fontSize: "30px", marginRight: "1rem" }}></i>
                             <b>New Equipment</b>
                         </Button>
-                        
                     </div>
                     <div className="col-sm-3 mt-5 mb-4 text-gred"></div>
-
                 </div>
-                
-                <Tabela 
-                    vetorEquipments={equipments} 
-                    handleRemove={removeEquipment} 
-                    handleShow={handleShow} 
-                    handleEdit={handleShowModalEdit} 
+
+                {/* <!--- Table equipments ---> */}
+                <Tabela
+                    vetorEquipments={equipments}
+                    handleRemove={removeEquipment}
+                    handleShow={handleShow}
+                    handleEdit={handleShowModalEdit}
                     filter={filter} />
 
                 {/* <!--- ModelAdd Box ---> */}
                 <div className="model_box">
-                    <ModalAdd                         
-                        show= {show}
-                        handleClose= {handleClose}                                           
+                    <ModalAdd
+                        show={show}
+                        handleClose={handleClose}
                     />
                 </div>
 
-                {/* <!--- ModelAdd Box ---> */}
+                {/* <!--- ModelEdit Box ---> */}
                 <div className="model_box">
-                    <ModalEdit                         
-                        showModalEdit= {showModalEdit}
-                        handleCloseModalEdit= {handleCloseModalEdit}
-                        obj= {objSelecionado}                                                            
+                    <ModalEdit
+                        showModalEdit={showModalEdit}
+                        handleCloseModalEdit={handleCloseModalEdit}
+                        obj={objSelecionado}
                     />
                 </div>
             </div>
         </div>
-
     )
 };
 export default Equipments;
